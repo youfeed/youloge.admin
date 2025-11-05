5b104f99ff302f824b2224fd4eb115585acb5bf1
youfeed <11247005@qq.com>
2025-11-05 18:33:49 +0800

    add

---
+++ public/json/menu.js
@@ -0,0 +1,498 @@
+{
+  "code": 0,
+  "msg": "",
+  "data": [
+    {
+      "name": "home",
+      "title": "主页",
+      "icon": "layui-icon-home",
+      "children": [
+        {
+          "title": "控制台",
+          "url": "/"
+        },
+        {
+          "title": "主页一",
+          "url": "/home/homepage1/keyword=123"
+        },
+        {
+          "title": "主页二",
+          "url": "/home/homepage2"
+        }
+      ]
+    },
+    {
+      "name": "component",
+      "title": "组件",
+      "icon": "layui-icon-component",
+      "children": [
+        {
+          "title": "栅格",
+          "children": [
+            {
+              "title": "等比例列表排列",
+              "url": "/component/grid/list"
+            },
+            {
+              "title": "按移动端排列",
+              "url": "/component/grid/mobile"
+            },
+            {
+              "title": "移动桌面端组合",
+              "url": "/component/grid/mobile-pc"
+            },
+            {
+              "title": "全端复杂组合",
+              "url": "/component/grid/all"
+            },
+            {
+              "title": "低于桌面堆叠排列",
+              "url": "/component/grid/stack"
+            },
+            {
+              "title": "九宫格",
+              "url": "/component/grid/speed-dial"
+            }
+          ]
+        },
+        {
+          "title": "按钮",
+          "url": "/component/button/"
+        },
+        {
+          "title": "表单",
+          "children": [
+            {
+              "title": "表单元素",
+              "url": "/component/form/element"
+            },
+            {
+              "title": "表单组合",
+              "url": "/component/form/group"
+            }
+          ]
+        },
+        {
+          "title": "导航",
+          "url": "/component/nav/"
+        },
+        {
+          "title": "标签页",
+          "url": "/component/tabs/"
+        },
+        {
+          "title": "选项卡",
+          "url": "/component/tab/"
+        },
+        {
+          "title": "进度条",
+          "url": "/component/progress/"
+        },
+        {
+          "title": "面板",
+          "url": "/component/panel/"
+        },
+        {
+          "title": "徽章",
+          "url": "/component/badge/"
+        },
+        {
+          "title": "时间线",
+          "url": "/component/timeline/"
+        },
+        {
+          "title": "动画",
+          "url": "/component/anim/"
+        },
+        {
+          "title": "辅助",
+          "url": "/component/auxiliar/"
+        },
+        {
+          "title": "通用弹层",
+          "children": [
+            {
+              "title": "功能演示",
+              "url": "/component/layer/list"
+            },
+            {
+              "title": "特殊示例",
+              "url": "/component/layer/special-demo"
+            },
+            {
+              "title": "风格定制",
+              "url": "/component/layer/theme"
+            }
+          ]
+        },
+        {
+          "title": "日期时间",
+          "children": [
+            {
+              "title": "功能演示一",
+              "url": "/component/laydate/demo1"
+            },
+            {
+              "title": "功能演示二",
+              "url": "/component/laydate/demo2"
+            },
+            {
+              "title": "设定主题",
+              "url": "/component/laydate/theme"
+            },
+            {
+              "title": "特殊示例",
+              "url": "/component/laydate/special-demo"
+            }
+          ]
+        },
+        {
+          "title": "静态表格",
+          "url": "component/table/static"
+        },
+        {
+          "name": "table",
+          "title": "表格组件",
+          "children": [
+            {
+              "title": "表格综合演示",
+              "url": "/component/table/"
+            },
+            {
+              "title": "列宽自动分配",
+              "url": "/component/table/auto"
+            },
+            {
+              "title": "赋值已知数据",
+              "url": "/component/table/data"
+            },
+            {
+              "title": "转化静态表格",
+              "url": "/component/table/tostatic"
+            },
+            {
+              "title": "自定义分页",
+              "url": "/component/table/resetPage"
+            },
+            {
+              "title": "开启单选框",
+              "url": "/component/table/radio"
+            },
+            {
+              "title": "双击单元格编辑",
+              "url": "/component/table/cellEdit"
+            },
+            {
+              "title": "加入表单元素",
+              "url": "/component/table/form"
+            },
+            {
+              "title": "设置单元格样式",
+              "url": "/component/table/style"
+            },
+            {
+              "title": "固定列",
+              "url": "/component/table/fixed"
+            },
+            {
+              "title": "解析任意数据格式",
+              "url": "/component/table/parseData"
+            },
+            {
+              "title": "行事件",
+              "url": "/component/table/onrow"
+            },
+            {
+              "title": "外部搜索",
+              "url": "/component/table/search"
+            },
+            {
+              "title": "设置初始排序",
+              "url": "/component/table/initSort"
+            },
+            {
+              "title": "单元格事件",
+              "url": "/component/table/cellEvent"
+            },
+            {
+              "title": "复杂表头",
+              "url": "/component/table/thead"
+            }
+          ]
+        },
+        {
+          "title": "分页",
+          "children": [
+            {
+              "title": "功能演示一",
+              "url": "/component/laypage/demo1"
+            },
+            {
+              "title": "功能演示二",
+              "url": "/component/laypage/demo2"
+            }
+          ]
+        },
+        {
+          "title": "树形表格",
+          "url": "/component/treeTable/"
+        },
+        {
+          "title": "下拉菜单",
+          "url": "/component/dropdown/"
+        },
+        {
+          "title": "上传",
+          "children": [
+            {
+              "title": "功能演示一",
+              "url": "/component/upload/demo1"
+            },
+            {
+              "title": "功能演示二",
+              "url": "/component/upload/demo2"
+            }
+          ]
+        },
+        {
+          "title": "穿梭框",
+          "url": "/component/transfer/"
+        },
+        {
+          "title": "树形组件",
+          "url": "/component/tree/"
+        },
+        {
+          "title": "颜色选择器",
+          "url": "/component/colorpicker/"
+        },
+        {
+          "title": "滑块组件",
+          "url": "/component/slider/"
+        },
+        {
+          "title": "评分",
+          "url": "/component/rate/"
+        },
+        {
+          "title": "轮播",
+          "url": "/component/carousel/"
+        },
+        {
+          "title": "流加载",
+          "url": "/component/flow/"
+        },
+        {
+          "title": "工具",
+          "url": "/component/util/"
+        },
+        {
+          "title": "代码修饰",
+          "url": "/component/code/"
+        },
+        {
+          "title": "国际化",
+          "url": "/component/i18n/"
+        }
+      ]
+    },
+    {
+      "name": "template",
+      "title": "页面",
+      "icon": "layui-icon-template",
+      "children": [
+        {
+          "title": "个人主页",
+          "url": "/template/personalpage"
+        },
+        {
+          "title": "通讯录",
+          "url": "/template/addresslist"
+        },
+        {
+          "title": "客户列表",
+          "url": "/template/caller"
+        },
+        {
+          "title": "商品列表",
+          "url": "/template/goodslist"
+        },
+        {
+          "title": "留言板",
+          "url": "/template/msgboard"
+        },
+        {
+          "title": "搜索结果",
+          "url": "/template/search"
+        },
+        {
+          "title": "注册",
+          "url": "/user/reg"
+        },
+        {
+          "title": "登入",
+          "url": "/user/login"
+        },
+        {
+          "title": "忘记密码",
+          "url": "/user/forget"
+        },
+        {
+          "title": "404 页",
+          "url": "/template/tips/404"
+        },
+        {
+          "title": "出错页",
+          "url": "/template/tips/error"
+        },
+        {
+          "title": "内嵌页面",
+          "spread": true,
+          "children": [
+            {
+              "title": "iframe 单独页面",
+              "url": "/iframe/link/demo"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "app",
+      "title": "应用",
+      "icon": "layui-icon-app",
+      "children": [
+        {
+          "title": "内容系统",
+          "children": [
+            {
+              "title": "文章列表",
+              "url": "/app/content/list"
+            },
+            {
+              "title": "分类管理",
+              "url": "/app/content/tags"
+            },
+            {
+              "title": "评论管理",
+              "url": "/app/content/comment"
+            }
+          ]
+        },
+        {
+          "name": "forum",
+          "title": "社区系统",
+          "children": [
+            {
+              "name": "list",
+              "title": "帖子列表",
+              "url": "/app/forum/list"
+            },
+            {
+              "name": "replys",
+              "title": "回帖列表",
+              "url": "/app/forum/replys"
+            }
+          ]
+        },
+        {
+          "title": "消息中心",
+          "url": "/app/message/"
+        },
+        {
+          "title": "工单系统",
+          "url": "/app/workorder/list"
+        }
+      ]
+    },
+    {
+      "name": "senior",
+      "title": "高级",
+      "icon": "layui-icon-senior",
+      "children": [
+        {
+          "title": "WebIM UI",
+          "url": "/senior/im/"
+        },
+        {
+          "title": "Echarts",
+          "children": [
+            {
+              "title": "折线图",
+              "url": "/senior/echarts/line"
+            },
+            {
+              "title": "柱状图",
+              "url": "/senior/echarts/bar"
+            },
+            {
+              "title": "地图",
+              "url": "/senior/echarts/map"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "user",
+      "title": "用户",
+      "icon": "layui-icon-user",
+      "children": [
+        {
+          "title": "网站用户",
+          "url": "/user/user/list"
+        },
+        {
+          "title": "后台管理员",
+          "url": "/user/administrators/list"
+        },
+        {
+          "title": "角色管理",
+          "url": "/user/administrators/role"
+        }
+      ]
+    },
+    {
+      "name": "set",
+      "title": "设置",
+      "icon": "layui-icon-set",
+      "children": [
+        {
+          "title": "系统设置",
+          "spread": true,
+          "children": [
+            {
+              "title": "网站设置",
+              "url": "/set/system/website"
+            },
+            {
+              "title": "邮件服务",
+              "url": "/set/system/email"
+            }
+          ]
+        },
+        {
+          "title": "我的设置",
+          "spread": true,
+          "children": [
+            {
+              "name": "info",
+              "title": "基本资料",
+              "url": "/set/user/info"
+            },
+            {
+              "title": "修改密码",
+              "url": "/set/user/password"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "about",
+      "title": "关于",
+      "icon": "layui-icon-auz",
+      "url": "/system/about"
+    }
+  ]
+}
---
+++ public/json/user/login.js
@@ -0,0 +1,8 @@
+{
+  "code": 0,
+  "msg": "登录成功",
+  "mark": "此处仅为静态模拟演示",
+  "data": {
+    "access_token": "TokenTestValue"
+  }
+}
