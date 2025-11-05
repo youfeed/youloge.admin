8b7ba5fe5b1ecd7b99a9870f4953d660dc054b19
youfeed <11247005@qq.com>
2025-11-05 18:29:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Youloge 开发者管理后台
+
+![Brightgreen](https://img.shields.io/badge/@-micateam-brightgreen.svg) ![Packagist](https://img.shields.io/packagist/v/youloge/webman.validate) ![Languages](https://img.shields.io/github/languages/top/youfeed/webman.validate.svg) ![Packagist Downloads](https://img.shields.io/packagist/dt/youloge/webman.validate) ![License ](https://img.shields.io/packagist/l/youloge/webman.validate)
+
+> Youloge平台数据全部开放的,如果你需要商户功能,审核功能需要有一台自己的服务器,可以管理更高权限的数据
+
+
+# 更新日志
+
+# 安装使用
+
+- 下载/克隆 仓库到服务器
+- 将代码部署到服务器 修改配置`.ini` 导入数据库 `.sql`
+
+# 仓库代码
+
+1. 后端为 [webman](https://www.workerman.net/webman) 速度快
+2. 前端为 [layuiAdmin](https://dev.layuion.com/themes/layuiAdmin) 商业化请尊重版权 购买授权
+
+
+
---
+++ app/controller/IndexController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\controller;
+
+use support\Request;
+
+class IndexController
+{
+    public function index(Request $request)
+    {
+        return <<<EOF
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  iframe {
+    border: none;
+    overflow: scroll;
+  }
+</style>
+<iframe
+  src="https://www.workerman.net/wellcome"
+  width="100%"
+  height="100%"
+  allow="clipboard-write"
+  sandbox="allow-scripts allow-same-origin allow-popups"
+></iframe>
+EOF;
+    }
+
+    public function view(Request $request)
+    {
+        return view('index/view', ['name' => 'webman']);
+    }
+
+    public function json(Request $request)
+    {
+        return json(['code' => 0, 'msg' => 'ok']);
+    }
+
+}
