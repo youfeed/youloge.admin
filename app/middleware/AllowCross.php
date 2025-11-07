b577a01736d118e8353f072bcb1373e0975fa69f
youfeed <11247005@qq.com>
2025-11-08 03:08:33 +0800

    add

--- app/middleware/AllowCross.php
+++ app/middleware/AllowCross.php
@@ -37,21 +37,30 @@ class AllowCross implements MiddlewareInterface
      */
     private function setCorsHeaders(Response $response, Request $request): Response
     {
-        $config = $this->allowConfig;
-        $origin = $request->header('origin', '');
-
-        // 3. 处理 Origin：只允许配置中的域名，避免冲突
-        // $allowOrigin = in_array($origin, $config['allow_origin']) ? $origin : '';
-        // 若不需要携带 Cookie（allow_credentials 为 false），可改为 $allowOrigin = '*'
-
-        // 4. 设置跨域头（严格遵循规范）
-        return $response->withHeaders([
-            'Access-Control-Allow-Origin' => $origin,
-            'Access-Control-Allow-Methods' => $config['allow_methods'],
-            'Access-Control-Allow-Headers' => $config['allow_headers'],
-            'Access-Control-Allow-Credentials' => $config['allow_credentials'] ? 'true' : 'false',
-            'Access-Control-Max-Age' => (string)$config['max_age'],
-            'Vary' => 'Origin' // 告诉浏览器根据 Origin 动态返回跨域头
+        $response = $request->method() == 'OPTIONS' ? response('') : $next($request);
+        $response->withHeaders([
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Origin' => $request->header('origin', '*'),
+            'Access-Control-Allow-Methods' => $request->header('access-control-request-method', '*'),
+            'Access-Control-Allow-Headers' => $request->header('access-control-request-headers', '*'),
         ]);
+
+        return $response;
+        // $config = $this->allowConfig;
+        // $origin = $request->header('origin', '');
+
+        // // 3. 处理 Origin：只允许配置中的域名，避免冲突
+        // // $allowOrigin = in_array($origin, $config['allow_origin']) ? $origin : '';
+        // // 若不需要携带 Cookie（allow_credentials 为 false），可改为 $allowOrigin = '*'
+
+        // // 4. 设置跨域头（严格遵循规范）
+        // return $response->withHeaders([
+        //     'Access-Control-Allow-Origin' => $origin,
+        //     'Access-Control-Allow-Methods' => $config['allow_methods'],
+        //     'Access-Control-Allow-Headers' => $config['allow_headers'],
+        //     'Access-Control-Allow-Credentials' => $config['allow_credentials'] ? 'true' : 'false',
+        //     'Access-Control-Max-Age' => (string)$config['max_age'],
+        //     'Vary' => 'Origin' // 告诉浏览器根据 Origin 动态返回跨域头
+        // ]);
     }
 }
