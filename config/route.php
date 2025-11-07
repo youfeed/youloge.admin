2ba50b6c76576a15a49a371badbbef6860e1e0bc
youfeed <11247005@qq.com>
2025-11-08 02:47:28 +0800

    use Webman\Http\Response;

--- config/route.php
+++ config/route.php
@@ -11,7 +11,7 @@
  * @link      http://www.workerman.net/
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
-use support\Response;
+use Webman\Http\Response;
 use Webman\Route;
 
 
