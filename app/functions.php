4c79603b696f119f92bb7f344a09516bd7268ca8
youfeed <11247005@qq.com>
2025-11-08 11:25:34 +0800

    add

--- app/functions.php
+++ app/functions.php
@@ -25,8 +25,8 @@ if(!function_exists('YoulogeEncrypt')){
         $iv = substr($cipher,0,16);$bin = substr($cipher,16);
         $outer_key = substr($secret,0,32);
         $inner_key = substr($secret,32,64);
-        $outer = openssl_decrypt($bin,'AES-256-CBC',$outer_key,1,$iv);
-        $inner = openssl_decrypt($outer,'AES-256-CBC',$inner_key,1,$iv);
+        $outer = openssl_decrypt($bin,'AES-256-CBC',$inner_key,1,$iv);
+        $inner = openssl_decrypt($outer,'AES-256-CBC',$outer_key,1,$iv);
         return json_decode($inner,true) ?? ['raw'=>$inner];
     } catch (\Throwable $th) {
         return [ 'err'=>$th->getCode(),'msg'=>$th->getMessage() ];
